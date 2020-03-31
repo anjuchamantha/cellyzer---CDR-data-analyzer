a6a6eaf5845e5e45ee0694ee29441cee4a102016
“wageesha_e_b” <wageesha26@gmail.com>
2020-03-31 22:05:59 +0530

    c

--- cellyzer/__init__.py
+++ cellyzer/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ['core', 'io', 'utils', 'tests']
 
 from .io import io_func, read_csv, to_csv, to_json, read_call, read_msg
-from .core import CallRecordsDF, MessagesDF, CellsDF
+from .core import CallRecord, MessageRecord, CellRecord
 from . import utils, io, core, visualization
 
 # __version__ = "0.5.3"
