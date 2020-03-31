43116497d0cb107109a01ad69992cfcce938f659
“wageesha_e_b” <wageesha26@gmail.com>
2020-03-31 15:51:37 +0530

    class structure

--- cellyzer/core.py
+++ cellyzer/core.py
@@ -19,41 +19,137 @@ import math
 #     def __init__(self):
 
 
-class CallRecordsDF:
-    # has CallRecords list with CallRecord objects
+# Classes for Records
+class Record:
+    def getsomething(self):
+        print ("in parent record class")
+
+
+class CallRecord(Record):
+
     def __init__(self, user, other_user, direction, duration, timestamp):
-        self.user = user,
-        self.other_user = other_user
-        self.direction = direction,
-        self.duration = duration,
-        self.timestamp = timestamp
+        self._user = user,
+        self._other_user = other_user
+        self._direction = direction,
+        self._duration = duration,
+        self._timestamp = timestamp
 
-    # bc>>> function to check whether 2 call records are equal
+    def get_user(self):
+        return self._user
+
+    def get_other_user(self):
+        return self._other_user
+
+    def get_direction(self):
+        return self._direction
+
+    def get_duration(self):
+        return self._direction
+
+    def get_timestamp(self):
+        return self._timestamp
 
 
-class MessagesDF:
-    # has Messages list with Message objects
+class MessageRecord(Record):
+
     def __init__(self, user, other_user, direction, length, timestamp):
-        self.user = user,
-        self.other_user = other_user
-        self.direction = direction,
-        self.length = length,
-        self.timestamp = timestamp
+        self._user = user,
+        self._other_user = other_user
+        self._direction = direction,
+        self._length = length,
+        self._timestamp = timestamp
 
-    # bc>>> function to check whether 2 messages are equal
+    def get_user(self):
+        return self._user
+
+    def get_other_user(self):
+        return self._other_user
+
+    def get_direction(self):
+        return self._direction
+
+    def get_duration(self):
+        return self._length
+
+    def get_timestamp(self):
+        return self._timestamp
 
 
-class CellsDF:
-    # has Messages dictionary with Cell objects : key is cell_id
+class CellRecord(Record):
     def __init__(self, cell_id, latitude, longitude):
-        self.cell_id = cell_id,
-        self.latitude = latitude,
-        self.longitude = longitude
+        self._cell_id = cell_id,
+        self._latitude = latitude,
+        self._longitude = longitude
 
-    # bc>>> get location
-    # bc>>> check 2 locations are equal
+    def get_cell_id(self):
+        return self._cell_id
+
+    def get_latitude(self):
+        return self._latitude
+
+    def get_longitude(self):
+        return self._longitude
 
 
+# classes for DataSet
+class DataSet:
+    records = []
+
+    def get_max(self):
+        print ("max")
+
+    def get_columns(self):
+        print ("columns")
+
+    def get_rows(self):
+        print ("rows")
+
+
+class CallDataSet(DataSet):
+    def get_close_contacts(self):
+        print("close contacts")
+
+    def get_most_active_time(self):
+        print ("most active time")
+
+    def get_call_details(self):
+        print ("call details")
+
+class MessageDataSet(DataSet):
+    def get_close_contacts(self):
+        print("close contacts")
+
+    def get_frequenct_conversations(self):
+        print("frequent conversations")
+
+
+class CellDataSet(DataSet):
+    def get_population(self , cell_id):
+        print("close contacts around = ", cell_id)
+
+
+# class User
+class User:
+    def __init__(self, contact_no):
+        self._contact_no = contact_no
+
+    def get_contact_no(self):
+        return self._contact_no
+
+    def get_trip(self):
+        print ("trips")
+
+    def get_home_location(self):
+        print ("home location = xxx . xxx")
+
+    def get_work_location(self):
+        print ("work location = xxx . xxx")
+
+    def get_ignored_calls(self):
+        print ("ignored calls = 111222333")
+
+
+# additional functions
 def graph():
     x = np.arange(0, math.pi * 2, 0.05)
     y = np.sin(x)
