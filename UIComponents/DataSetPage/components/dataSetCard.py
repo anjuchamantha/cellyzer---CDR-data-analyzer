26b40f44ea5c861d2d187fd04197ba87cc091160
Anju Chamantha <chamantha97anju@gmail.com>
2020-04-19 15:50:43 +0530

    Adding UI Components for calling cellyzer functions

---
+++ UIComponents/DataSetPage/components/dataSetCard.py
@@ -0,0 +1,23 @@
+import dash_bootstrap_components as dbc
+import dash_html_components as html
+
+
+def DataSetCard(name, records="-", type="call"):
+    return dbc.Card(
+        children=[
+            dbc.CardHeader(html.H5(name, style={"text-align": "center"})),
+            dbc.CardBody(
+                [
+                    html.H5(records, style={"font-size": 50, "margin-bottom": 0}),
+                    html.P(
+                        "records", style={}
+                    ),
+                    html.Br(),
+                    dbc.Button("Visit DataSet", color="primary", href="/dataset/" + type),
+                ],
+                style={"text-align": "center"}
+            ),
+        ],
+        style={"width": 300, "margin-right": 20, "margin-bottom": 20}
+    )
+
---
+++ UIComponents/index.py
@@ -0,0 +1,97 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+from Call.callPage import CallPage
+from DataSetPage.datasetPage import DataSetPage
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "16rem",
+    "padding": 20,
+}
+
+sidebar = html.Div(
+    [
+        html.H2("CELLYZER", className="display-6", style={"text-align": "center"}),
+        html.Hr(),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/home", id="home"),
+                dbc.NavLink("DataSets", href="/dataset", id="dataset"),
+                dbc.NavLink("Analytics", href="/analytics", id="analytics"),
+
+            ],
+            vertical=True,
+            pills=True,
+        ),
+
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+
+# this callback uses the current pathname to set the active state of the
+# corresponding nav link to true, allowing users to tell see page they are on
+@app.callback(
+    [Output(f"{i}", "active") for i in ["home", "dataset", "analytics"]],
+    [Input("url", "pathname")],
+)
+def toggle_active_links(pathname):
+    if pathname == "/" or pathname == "/home":
+        # Treat page 1 as the homepage / index
+        return True, False, False
+    if pathname == "/dataset":
+        return False, True, False
+    if pathname == "/analytics":
+        return False, False, True
+
+
+datasetpage = DataSetPage()
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname in ["/", "/home"]:
+        return html.P("HomePage")
+    elif pathname == "/dataset":
+        return datasetpage
+    elif pathname == "/analytics":
+        return html.P("Analytics Page")
+    elif pathname == "/dataset/call":
+        return CallPage()
+
+
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+if __name__ == "__main__":
+    app.run_server(port=8888)
