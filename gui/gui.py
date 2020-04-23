505790c4c78fbe84033484fa92fad7020e085f48
malithbc <47839907+malithbc@users.noreply.github.com>
2020-04-23 12:39:32 +0530

    visualize message dataset

--- gui/gui.py
+++ gui/gui.py
@@ -43,31 +43,6 @@ app.layout = html.Div([
 ])
 
 
-def parse_data(contents, filename):
-    content_type, content_string = contents.split(',')
-
-    decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV or TXT file
-            df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')))
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded))
-        # elif 'txt' or 'tsv' in filename:
-        #     # Assume that the user upl, delimiter = r'\s+'oaded an excel file
-        #     df = pd.read_csv(
-        #         io.StringIO(decoded.decode('utf-8')), delimiter = r'\s+')
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-        ])
-
-    return df
-
-
 ## Front page
 index_page = html.Div([
     html.H1(className='index_page_CELLYZER',
