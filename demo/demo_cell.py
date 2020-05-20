e13cead640635c17fce7ad77da39cde1d14509f5
Anju Chamantha <chamantha97anju@gmail.com>
2020-05-21 03:29:39 +0530

    refactoring demo cell file

--- demo/demo_cell.py
+++ demo/demo_cell.py
@@ -9,23 +9,27 @@ antenna_file_path = "demo_datasets/test_data/antennas.csv"
 callDataSet = cz.read_call(call_file_path)
 
 antennaDataSet = cz.read_cell(antenna_file_path, call_dataset_obj=callDataSet, file_type='csv')
-record = antennaDataSet.get_cell_records(cell_id=1)
-print("cell id - ", record.get_cell_id())
 
-population = antennaDataSet.get_population()
+# print antenna data set
+cz.utils.print_dataset(antennaDataSet, name="Antenna Data set")
+
+# get a record of a given cell id
+record = antennaDataSet.get_cell_records(cell_id=1)
+print("Record of cell %s : %s \n" % (1, record.__dict__))
+
 print(">> population around cell")
-print(population)
+population = antennaDataSet.get_population()
+# print(population)
 cz.utils.tabulate_list_of_dictionaries(population)
 cz.visualization.cell_population_visualization(population)
 
-call_made_locations = antennaDataSet.get_trip_details("7331c02864", console_print=True, tabulate=True)
-
+print(">> Trip details of user : %s" % "7331c02864")
+call_made_locations = antennaDataSet.get_trip_details("7331c02864", console_print=False, tabulate=True)
 cz.visualization.trip_visualization(call_made_locations, notebook=False)
 
-all_records = antennaDataSet.get_cell_records()
-print('len - records = ', len(all_records))
-
 test = callDataSet.get_records()
+print(">> Unique users")
 print(antennaDataSet.get_unique_users_around_cell(test))
 
-print(antennaDataSet.check_user_location_matches_cell('123', 10))
+print(">> Is %s recorded in cell %s" % ("123", 10))
+print(antennaDataSet.check_user_location_matches_cell(contact_no='123', cell_id=10))
