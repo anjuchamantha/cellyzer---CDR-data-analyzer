cf3ccaf22d1f600a54e266cb05a521bc700b9483
Anju Chamantha <chamantha97anju@gmail.com>
2020-06-15 11:02:03 +0530

    pip cellyzer 1.1.1

--- cellyzer/__init__.py
+++ cellyzer/__init__.py
@@ -2,6 +2,6 @@ __all__ = ['core', 'io', 'utils', 'tests']
 
 from .io import io_func, read_csv, to_csv, to_json, read_call, read_msg, read_cell
 from .core import CallRecord, MessageRecord, CellRecord, CallDataSet, MessageDataSet, CellDataSet, User
-from . import utils, io, core, visualization, tools
+from . import utils, io, core, visualization, tools, matrix
 
 # __version__ = "0.5.3"
